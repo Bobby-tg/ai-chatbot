d2352b494cbabf46a679207fe8c9a4cb20002f3f
Bobby-tg <166282590+Bobby-tg@users.noreply.github.com>
2024-12-03 14:54:23 +0000

    Test Four

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,19 @@
-import streamlit as st
+#AIzaSyDZhQ3-_z06g4MVtLLmMwHAykwxSr7WGrc
 
-st.title("🐧 PM 2.5 Chatbot App")
-
+import streamlit as st 
+st.title("🐧 My Chatbot App") 
 st.subheader("Conversation") 
+
 # Initialize session state for storing chat history 
-
 if "chat_history" not in st.session_state: 
-    st.session_state.chat_history = [] # Initialize with an empty list 
+	st.session_state.chat_history = [] # Initialize with an empty list
 
-# Display the user input 
-if user_input := st.text_input("You: ", placeholder="Type your message here..."): 
-    st.session_state.chat_history.append(user_input) 
-    
-# Display all messages using st.write 
+# Display all chat messages 
 for message in st.session_state.chat_history: 
-    st.write(message)
+	with st.chat_message("user"): 
+		st.markdown(message) 
+
+# Capture user input and append to chat history 
+if prompt := st.chat_input("Type your message here ..."):
+	st.session_state.chat_history.append(prompt) 
+	st.chat_message("user").markdown(prompt)
