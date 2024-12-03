c76dcf51c2d58975005fa55f104d44600bc2a604
Bobby-tg <166282590+Bobby-tg@users.noreply.github.com>
2024-12-03 13:28:47 +0000

    Test 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,2 @@
 import streamlit as st
 
-st.title("🎈 PM2.5 Chatbot App")
-
-
-# Display the user input 
-if user_input := st.text_input("You: ", placeholder="Type your message here..."): 
-    st.write(f"User Input: {user_input}")
